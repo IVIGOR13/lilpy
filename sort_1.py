05e1c96ed6d9d85fceabc0dc51907845d38076a3
Igor Ivanov <31894518+IVIGOR13@users.noreply.github.com>
2020-04-04 00:10:21 +0300

    Create sort_1.py

---
+++ sort_1.py
@@ -0,0 +1,3 @@
+# Sorting
+# Сортировка
+print(list(map(int, ','.join(str(','.join(str(x) for x in x)) for x in [[r for r in e ] for e in [[[[arr[arr1.index(q)] for w in range(len(arr1)) if q == arr1[w]] for q in range(len(arr1)) if q in arr1] for arr1 in [[ len([ 1 for j in range(len(arr)) if i!=j and arr[j] <= arr[i]]) for i in range(len(arr))]]] for arr in [list(map(int, input().split(' ')))]][0][0]]).split(','))))
